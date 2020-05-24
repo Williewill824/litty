351542cc62266bf8d331e04c189c603f51af868d
Willie Gomez <willie.gomezjr12@gmail.com>
2020-05-24 15:36:44 -0400

    update2

--- generator.js
+++ generator.js
@@ -5,8 +5,8 @@ function generateMarkdown(data) {
 
     fs.writeFile(
         filename,
-    `(https://img.shields.io/badge/Rutgers-100.0%25-red)${"# " + data.title}
-    [${data.tableOfContents[0]}](#Installations)]
+    `![](https://img.shields.io/badge/Rutgers-100.0%25-red)${"# " + data.title}
+    [${data.tableOfContents[0]}](#Installations)
     [${data.tableOfContents[1]}](#Usage)
     [${data.tableOfContents[2]}](#Credits)
     [${data.tableOfContents[3]}](#License)
